6759be13219e367dfe1ebb713d211fe5932fd3e0
Alexandr Zanin <bsalexandrzanin@gmail.com>
2022-06-10 11:12:12 +0300

    global_link_url and new view

--- skills/models.py
+++ skills/models.py
@@ -1,11 +1,7 @@
 from django.db import models
+from django.urls import reverse
 
 
-# id - int
-# name_skill - text
-# created_at- DateTime
-# update_at - DateTime
-# photo - Image
 class Skill(models.Model):
     name_skill = models.CharField(max_length=50, verbose_name='Название навыка')
     description = models.TextField(blank=True, verbose_name='Описание')
@@ -14,6 +10,9 @@ class Skill(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Навык поулчен?')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def get_absolute_url(self):
+        return reverse('view_skills', kwargs={'skill_id': self.pk})
 
     def __str__(self):
         return self.name_skill
@@ -26,6 +25,9 @@ class Skill(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=50, verbose_name='Название категории', db_index=True)
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
 
     def __str__(self):
         return self.title
--- skills/views.py
+++ skills/views.py
@@ -1,23 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Skill, Category
 
 
 def index(request):
     skill = Skill.objects.all()
-    categories = Category.objects.all()
     context = {'skills': skill,
                'name_skill': 'Список навыков',
-               'categories': categories,}
+               }
     return render(request, 'skills/index.html', context=context)
 
 
 def get_category(request, category_id):
     skills = Skill.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     return render(request, template_name='skills/category.html', context={
         'skills': skills,
-        'categories': categories,
         'category': category
     })
+
+
+def view_skills(request, skill_id):
+    #skill_item = Skill.objects.get(pk=skill_id)
+    skill_item = get_object_or_404(Skill, pk=skill_id)
+    return render(request, 'skills/view_skills.html', {'skill_item': skill_item})
